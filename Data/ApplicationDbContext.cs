1c18ab1b672050cf6e1c3cb082be7c35a5abdba7
vinhvhgcs <99672763+vinhvhgcs@users.noreply.github.com>
2022-04-15 12:41:14 +0700

    Add file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Web1640.Models;
+
+namespace Web1640.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Comments> Comments { get; set; }
+        public DbSet<Config> Config { get; set; }
+        public DbSet<Idea> Idea { get; set; }
+        public DbSet<Member> Member { get; set; }
+        public DbSet<PermissionRole> PermissionRole { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<Staff> Staff { get; set; }
+    }
+}
